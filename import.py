c89bfc063924bf0cc27ce6371e527699ac693230
CaandRev <68998429+CaandRev@users.noreply.github.com>
2022-01-21 12:21:50 +0100

    New try to use GitHub

--- import.py
+++ import.py
@@ -4,6 +4,7 @@ from fuzzywuzzy import fuzz
 # from fuzzywuzzy import process
 
 starttime=runtime.starttime()
+os.chdir('C:\\Users\\caspe\\OneDrive\\Prog\\Aksje prosjekt\\WIP')
 cwd = os.getcwd()
 
 # thisyear=datetime.datetime.now().year
@@ -37,7 +38,7 @@ with open('ansatt_justert.csv', 'r', newline='', encoding= 'utf-8-sig') as filew
             if reads[0] == '':
                 continue
             if comp == 100:
-                # print('Der er et match yeahh!!! Navnet er: ',reads[0], ' ; ', name,'%.1f' % comp,'\n')
+                # print('Der er et match yeahh!!! Navnet er: ',reads[0], ' ; ', name,'%.1f' % comp,'\\n')
                 newans.append(writes)
                 newans[count].extend(reads[4:6])
                 newname = name = str(writes[1]+' '+writes[2]).upper()
@@ -51,9 +52,9 @@ with open('ansatt_justert.csv', 'r', newline='', encoding= 'utf-8-sig') as filew
                 for n in range(lenght):
                     
                     if x[n].upper() in y[n].upper():
-                        namecount = namecount + 1
+                        namecount += 1
                     if namecount/lenght == 1:
-                        # print('Der er et match yeahh!!! Navnet er: ',reads[0], ' ; ', name,'%.1f' % comp,'\n')
+                        # print('Der er et match yeahh!!! Navnet er: ',reads[0], ' ; ', name,'%.1f' % comp,'\\n')
                         newans.append(writes)
                         newans[count].extend(reads[4:6])
                         newname = name = str(writes[1]+' '+writes[2]).upper()
@@ -63,33 +64,34 @@ with open('ansatt_justert.csv', 'r', newline='', encoding= 'utf-8-sig') as filew
 with open('newfile.csv', 'w', newline ='', encoding='utf-8-sig') as fhand:    
     f= csv.writer(fhand)
    
-    newlst=[['Ansattnr','Etternavn','Fornavn','E-Post','Avdeling','Gruppe','Kontor','Inntektskategori','Utdannet år','Stillingsprosent','Type','Startet','Sluttet','Tjenestetid', header[0],header[1],header[0],header[1],header[0],header[1],header[0],header[1],header[0],header[1],header[0],header[1],header[0],header[1],header[0],header[1],header[0],header[1],header[0],header[1],header[0],header[1],header[0],header[1],header[0],header[1],header[0],header[1]]]
+    newlst=[['Ansattnr','Etternavn','Fornavn','E-Post','Avdeling','Gruppe','Kontor','Inntektskategori','Utdannet år','Stillingsprosent','Type','Startet','Sluttet','Tjenestetid', 'År','Antal','Type aksje']]
     for i in newans:            
         if i not in newlst:
             newlst.append(i)
     
     f.writerows(newlst)
 
-with open('newfile.csv', 'r', newline='', encoding='utf-8-sig') as f:
-    count = 0
-    fhand = csv.reader(f)
-    for i in fhand:
-        count+=1
-        lenght=len(i)
+# Bør utgår i tilfelle det blir en lang liste
+# with open('newfile.csv', 'r', newline='', encoding='utf-8-sig') as f:
+#     count = 0
+#     fhand = csv.reader(f)
+#     for i in fhand:
+#         count+=1
+#         lenght=len(i)
             
-        if lenght < 42:
-            item = i[14:lenght+1]
-            del i[14:lenght+1]
-            for j in range(42-lenght):
-                i.append('N/A')
-            for j in item:
-                i.append(j)
-            lst.append(i)
-            # print(i,len(i))
-        else:
-            lst.append(i)
+#         if lenght < 42:
+#             item = i[14:lenght+1]
+#             del i[14:lenght+1]
+#             for j in range(42-lenght):
+#                 i.append('N/A')
+#             for j in item:
+#                 i.append(j)
+#             lst.append(i)
+#             # print(i,len(i))
+#         else:
+#             lst.append(i)
        
-with open('resultat.csv', 'w', newline='', encoding='utf-8-sig') as f:
+with open('resultatlong.csv', 'w', newline='', encoding='utf-8-sig') as f:
     fhand = csv.writer(f)   
     fhand.writerows(lst)
     
